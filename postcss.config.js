08dc937858f84c2e76f3158c32f6481bd3762ad0
Sıtkı Bağdat <sbagdat@gmail.com>
2021-02-18 04:35:19 +0300

    Add tailwindcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
     require('postcss-preset-env')({
-
+      stage: 1,
       browsers: 'last 2 versions',
     }),
-  ],
-};
+  ]
+}
